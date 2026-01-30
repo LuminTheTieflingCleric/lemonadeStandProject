e67403fdfebfafc1e5191548b291bf5c9099d1de
nb557435 <nb557435@georgetownisd.org>
2026-01-30 13:39:12 -0600

    It change a little bit.
    its gust adding a price.

---
+++ score_analyzer_brewer.py
@@ -0,0 +1,9 @@
+score = []
+s=(input(int("how meny scores do you whont to enter")))
+print(s)
+if s >= 90:
+    print("Exolint class prformince")
+elif s >= 80:
+    print("good class prformce")
+elif s >=70:
+    print("class prformince passing but coud inprove. let's study more.")
