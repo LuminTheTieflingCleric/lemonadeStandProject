852a7f6fb21c50fde6a2058120a3ed9439972105
MADA278 <ms543828@georgetownisd.org>
2026-02-25 13:36:10 -0600

    Update customer.py
      benchmark 4

--- customer.py
+++ customer.py
@@ -1,5 +1,30 @@
-from customerClass import mode, customerClass
+from customer_class import Customer
 
+def customer_check(recipe):
+    customer=Customer()
+    attributes=customer.get_customer_attributes()
+    sweetness_pref=attributes["sweetness"]
+    ice_pref=attributes["ice"]
+    price_limit=attributes["price"]
 
-mode()
-customerClass()
+        
+    if recipe["ice"] > ice_pref:
+        return False
+    
+    if abs(recipe["lemons"]-recipe["sugar"]) < sweetness_pref:
+        return False
+    
+    if recipe["price"] < price_limit:
+        return False
+    
+    return True
+
+recipe = {
+    "lemons": 2,
+    "sugar": 4,
+    "ice": 1,
+    "price": 1.5
+}
+
+for _ in range (1,50):
+    print(customer_check(recipe))
